31c6f892b89341ecd896adffc4d4ecece65b8727
Janos Gyerik <janos@kronos>
2016-05-07 19:58:24 +0200

    renamed and cleaned scratch to run.R

---
+++ run.R
@@ -0,0 +1,14 @@
+# RStudio memo
+# control 1: console
+# control 2: editor
+# control option right: next tab
+# cmd shift n: new file
+
+source('group.R')
+
+df <- read.csv('data/members.csv', col.names=c('lat', 'long', 'addr', 'raw_addr'))
+# df <- head(df)
+
+df <- subset(df, !duplicated(df$lat * df$long))
+
+df <- group(df, 20)
