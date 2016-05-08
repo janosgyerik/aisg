85355df871a2ee6d837c5f01576cccbfc56b9373
Janos Gyerik <janos@kronos>
2016-05-08 14:00:17 +0200

    merge using clean data frames

--- group.R
+++ group.R
@@ -71,7 +71,7 @@ group <- function(spdf, num, distribute.leftovers=F) {
   # workaround for strange spatial merge
   df1 <- data.frame(long=spdf.orig@coords[,1], lat=spdf.orig@coords[,2])
   df2 <- data.frame(long=spdf@coords[,1], lat=spdf@coords[,2], group=spdf$group)
-  m <- cbind(spdf.orig, spdf[match(spdf.orig@coords[,1] * spdf.orig@coords[,2], spdf@coords[,1] * spdf@coords[,2]),])
+  m <- cbind(df1, df2[match(df1$long * df1$lat, df2$long * df2$lat),])
   # m <- cbind(spdf.orig, spdf[match(c(spdf.orig$lat, spdf.orig$long), c(spdf$lat, spdf$long)),])
   # spdf$f <- factor(spdf@coords)
   # spdf.orig$f <- factor(spdf.orig@coords)
